ad55e4e87b3d34d3e997a7bbba1103ea806a1985
Amber Aldrich <github@amberaldrich.com>
2016-05-17 23:40:41 -0500

    added methods

--- Review/src/fun/Train.java
+++ Review/src/fun/Train.java
@@ -2,5 +2,27 @@ package fun;
 
 public class Train {
 
+	private int cars;
+	private int locamotive;
+	private double rate;
+	private int passangers;
+	private int passangersPerCar;
 	
+	
+	public Train(int Passangers, double miles, double pricePerMile) {
+		cars = 0;
+		locamotive = 1;
+		rate = pricePerMile * miles;
+	}
+	public double rate(){
+		return rate;
+	}
+	public int trainCars(int passengersPerCar){
+		cars = passangers * passangersPerCar;
+		return cars;
+	}
+	public int locamotives(int carsPerLocamotive){
+		locamotive = cars * carsPerLocamotive;
+		return locamotive;
+	}
 }
